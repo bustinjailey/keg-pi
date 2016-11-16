575f5340c225ef7d71c90414b938f0fb0823b78f
Justin Bailey <bustinjailey@gmail.com>
2016-11-16 01:36:13 +0000

    Add beginning of an install script and fix the wrong casing in a file name

---
+++ beerweb/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo 'Checking for nodejs'
+
+if ! node_loc="$(type -p "node")" || [ -z "$node_loc" ]; then
+  echo 'nodejs is not installed... installing.'
+  curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+  sudo apt-get install -y nodejs build-essential
+fi
+
+echo 'Installing beerweb backend API'
+cd ./server
+npm install
+cd ..
+
+echo 'Installing beerweb frontend'
+cd ./ui
+npm install
+cd ..
+
