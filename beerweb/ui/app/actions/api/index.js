32f92382f7a260737cab1e4ca60d46b62d0addc6
Bailey, Justin <bustinjailey@gmail.com>
2017-02-21 15:44:34 -0800

    Get rid of old, bad UI

--- beerweb/ui/app/actions/api/index.js
+++
@@ -1,9 +0,0 @@
-export * from './BreweryApiActions';
-export * from './BeerApiActions';
-export * from './KegApiActions';
-
-export function shouldFetchArrayType(arrayFromState) {
-  return !!(!arrayFromState || arrayFromState.length === 0);
-}
-
-export const apiHost = "localhost";
--- beerweb/ui/app/actions/constants/PageNames.js
+++
@@ -1,5 +0,0 @@
-export const PageNames = {
-  KEG_LIST: 'KEG_LIST',
-  BREWERY_LIST: 'BREWERY_LIST',
-  BEER_LIST: 'BEER_LIST'
-};
